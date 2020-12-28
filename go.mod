13560c9dc73f11e99e572e733a5400ae1cd1c306
daiyang <dystargate@gmail.com>
2020-12-28 17:16:47 +0800

    Add utils/db

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.12
 
 require (
 	github.com/astaxie/beego v1.12.3
+	github.com/jinzhu/gorm v1.9.16
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	go.uber.org/zap v1.16.0
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
 )
---
+++ utils/db.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+var globalDB *gorm.DB
+
+func InitDB() error {
+	user := GetConf().String("db::user")
+	password := GetConf().String("db::password")
+	host := GetConf().String("db::host")
+	port := GetConf().String("db::port")
+	dbName := GetConf().String("db::dbname")
+	parameters := GetConf().String("db::parameters")
+
+	mysqlUrl := DSN(user, password, host, port, dbName, parameters)
+	db, err := gorm.Open(mysqlUrl)
+	if err != nil {
+		return err
+	}
+
+	//连接池
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+	db.DB().SetConnMaxLifetime(time.Hour)
+
+	globalDB = db
+
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return globalDB
+}
