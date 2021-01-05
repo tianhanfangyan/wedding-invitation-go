e64a1e40cfdbd44c1d8d0ef504c005a52ba8c102
daiyang <dystargate@gmail.com>
2021-01-05 17:28:05 +0800

    Add utils/pagination

---
+++ utils/pagination.go
@@ -0,0 +1,22 @@
+package utils
+
+// 获取分页的参数
+func GetPage(pageNumber, pageSize int) (int, int) {
+	var pageNumberResult, pageSizeResult int
+
+	// 页码
+	if pageSize != 0 {
+		pageSizeResult = pageSize
+	} else {
+		pageSize = 10
+	}
+
+	// 页数
+	if pageNumber != 0 {
+		pageNumberResult = (pageNumber - 1) * pageSizeResult
+	} else {
+		pageNumberResult = 1
+	}
+
+	return pageNumberResult, pageSizeResult
+}
